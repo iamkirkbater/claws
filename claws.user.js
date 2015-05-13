ab3974fca0794a6da28d0b3e3339f79c0cc1fb05
Kirk Bater <kxbtwc@rit.edu>
2015-05-13 11:42:10 -0400

    Added Userscript file.

---
+++ claws.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         CLAWS overhaul
+// @namespace    http://clawsstage.rit.edu/
+// @version      0.1
+// @description  CSS and Javascript Tweaks for the RIT CLAWS webhosting application
+// @author       Kirk Bater
+// @include      *://clawsstage.rit.edu/*
+// @grant        none
+// ==/UserScript==
+
+$('.ui-corner-all').removeClass('ui-corner-all');
